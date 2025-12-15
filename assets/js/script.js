7bc5846024e560af07f8ffbe3721a4d57b34bb31
Giorgia Formicola <giorgia.formicola97@gmail.com>
2025-12-15 16:51:48 +0100

    Players card update

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,23 @@ const createNumbersBoard = function () {
 
 createNumbersBoard();
 
+const createPlayerCard = function () {
+  const playerSection = document.querySelector("#player");
+  const playerCard = document.createElement("div");
+  playerCard.classList.add("card");
+  for (let i = 0; i < 24; i++) {
+    const playerCardCell = document.createElement("div");
+    playerCardCell.textContent = Math.ceil(Math.random() * 90);
+    playerCardCell.classList.add("playerCell");
+    playerCard.appendChild(playerCardCell);
+    playerSection.appendChild(playerCard);
+  }
+};
+
+createPlayerCard();
+createPlayerCard();
+createPlayerCard();
+
 const pullButton = document.querySelector("button");
 
 pullButton.addEventListener("click", function (e) {
@@ -25,6 +42,13 @@ pullButton.addEventListener("click", function (e) {
   const allNumberCells = document.querySelectorAll("#numbers-board div");
 
   allNumberCells.forEach((cell) => {
+    if (parseInt(cell.textContent) === randomNumb) {
+      cell.classList.add("pulledNumbers");
+    }
+  });
+
+  const allPlayerCells = document.querySelectorAll(".playerCell");
+  allPlayerCells.forEach((cell) => {
     if (parseInt(cell.textContent) === randomNumb) {
       cell.classList.add("pulledNumbers");
     }
